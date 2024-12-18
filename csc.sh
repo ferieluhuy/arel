1d6ceae0c9b4c6433cfaac16d7fedea12126fad5
ferieluhuy <erielnugraha08@gmail.com>
2024-12-18 21:21:01 +0700

    Add files via upload

--- csc.sh
+++ csc.sh
@@ -1,49 +1,42 @@
 #!/bin/bash
 
-CISCO_IP="192.168.73.131" # Ganti dengan IP perangkat Cisco
+CISCO_IP="192.168.73.131"  # IP perangkat Cisco
 CISCO_PORT="30003"
-UBUNTU_ROUTE1="192.168.200.0/24"  # Jaringan tujuan pertama
-UBUNTU_GATEWAY1="192.168.12.2"    # Gateway pertama
-UBUNTU_ROUTE2="192.168.12.0/24"   # Jaringan tujuan kedua (opsional)
-UBUNTU_GATEWAY2="192.168.12.1"    # Gateway kedua
 
-# Konfigurasi Telnet dengan Expect untuk Cisco
+# Konfigurasi Cisco: VLAN, Trunking, dan Akses Port DHCP
 expect -c "
 spawn telnet $CISCO_IP $CISCO_PORT
 send \"enable\r\"
 expect \"#\"
 send \"configure terminal\r\"
 expect \"(config)#\"
+
+# Membuat VLAN 10
 send \"vlan 10\r\"
 expect \"(config-vlan)#\"
-send \"name Vlan10\r\"
+send \"name VLAN10\r\"
 expect \"(config-vlan)#\"
 send \"exit\r\"
 
-# Konfigurasi interface VLAN
-send \"interface vlan 10\r\"
-expect \"(config-if)#\"
-send \"ip address 192.168.12.1 255.255.255.0\r\"
-expect \"(config-if)#\"
-send \"no shutdown\r\"
-expect \"(config-if)#\"
-send \"exit\r\"
-
-# Konfigurasi mode trunk pada interface
-send \"interface e0/0\r\"  # Ganti dengan interface yang benar
+# Mengonfigurasi interface trunk (port e0/0)
+send \"interface e0/0\r\"  # Port trunk
 expect \"(config-if)#\"
 send \"switchport mode trunk\r\"
 expect \"(config-if)#\"
-send \"switchport trunk allowed vlan 10\r\"  # Mengizinkan VLAN 10 di trunk
+send \"switchport trunk allowed vlan 10\r\"  # Mengizinkan VLAN 10
 expect \"(config-if)#\"
 send \"exit\r\"
 
-# Menambahkan IP Route di Cisco
-expect \"(config)#\"
-send \"ip route $UBUNTU_ROUTE1 $UBUNTU_GATEWAY1\r\"
-expect \"(config)#\"
-send \"ip route $UBUNTU_ROUTE2 $UBUNTU_GATEWAY2\r\"
-expect \"(config)#\"
+# Mengonfigurasi port akses VLAN 10 (kabel DHCP)
+send \"interface e0/1\r\"  # Ganti ke port yang terhubung ke kabel DHCP
+expect \"(config-if)#\"
+send \"switchport mode access\r\"
+expect \"(config-if)#\"
+send \"switchport access vlan 10\r\"
+expect \"(config-if)#\"
+send \"exit\r\"
+
+# Menyimpan konfigurasi
 send \"end\r\"
 expect \"#\"
 send \"write memory\r\"
@@ -52,19 +45,5 @@ send \"exit\r\"
 expect eof
 "
 
-# Menambahkan IP Route di Ubuntu
-echo "[INFO] Menambahkan IP Route di Ubuntu menggunakan 'via'..."
-sudo ip route add $UBUNTU_ROUTE1 via $UBUNTU_GATEWAY1
-sudo ip route add $UBUNTU_ROUTE2 via $UBUNTU_GATEWAY2
-
-# Menyimpan IP Route agar permanen di Ubuntu
-echo "[INFO] Menyimpan IP Route di Ubuntu agar permanen..."
-echo "up ip route add $UBUNTU_ROUTE1 via $UBUNTU_GATEWAY1" | sudo tee -a /etc/network/interfaces > /dev/null
-echo "up ip route add $UBUNTU_ROUTE2 via $UBUNTU_GATEWAY2" | sudo tee -a /etc/network/interfaces > /dev/null
-
-# Verifikasi konfigurasi routing
-echo "[INFO] Routing aktif saat ini:"
-ip route
-
 # Pesan selesai
-echo "[SUCCESS] Konfigurasi Cisco dan penambahan IP Route ke Ubuntu selesai!"
+echo "[SUCCESS] VLAN 10 telah ditambahkan, trunking aktif, dan kabel DHCP dipindahkan ke VLAN 10."
