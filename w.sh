201926ed89af3b8e96ea5afdc53a9907aea78264
ferieluhuy <erielnugraha08@gmail.com>
2025-01-07 10:38:11 +0700

    Add files via upload

---
+++ w.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CISCO_IP="192.168.72.131"  # IP perangkat Cisco
+CISCO_PORT="30003"
+spawn telnet $CISCO_IP $CISCO_PORT
+set timeout 20
+
+expect ">" { send "enable\r" }
+expect "#" { send "configure terminal\r" }
+expect "(config)#" { send "interface Ethernet0/1\r" }
+expect "(config-if)#" { send "switchport mode access\r" }
+expect "(config-if)#" { send "switchport access vlan 10\r" }
+expect "(config-if)#" { send "no shutdown\r" }
+expect "(config-if)#" { send "exit\r" }
+expect "(config)#" { send "interface Ethernet0/0\r" }
+expect "(config-if)#" { send "switchport trunk encapsulation dot1q\r" }
+expect "(config-if)#" { send "switchport mode trunk\r" }
+expect "(config-if)#" { send "no shutdown\r" }
+expect "(config-if)#" { send "exit\r" }
+expect "(config)#" { send "exit\r" }
+expect "#" { send "exit\r" }
+expect eof
+EOF
+
+# Pesan selesai
+echo "[SUCCESS] VLAN 10 telah ditambahkan, trunking aktif, dan kabel DHCP dipindahkan ke VLAN 10."
